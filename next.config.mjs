b3e21c5d1a6b42cd8e2057d9157f8e042b0a3304
Neyamat Ullah <neyamat7.ullah@gmail.com>
2026-08-12 08:45:21 +0600

    refactor: remove static export configuration and image optimization settings from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   output: "export",
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
